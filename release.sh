9ef5b6b1cda3c88b5d04dc01af67f0597b822795
Karol Baczewski <braxnu@gmail.com>
2023-01-29 15:50:16 +0000

    separate startup script

--- release.sh
+++ release.sh
@@ -3,14 +3,3 @@ set -e
 
 npm run build
 docker build -t medyk-lab .
-
-# docker run \
-#   --rm \
-#   -v=$(pwd)/data.json:/opt/data.json \
-#   -v=$(pwd)/users.json:/opt/users.json \
-#   -e='JWT_SECRET=very-secret' \
-#   -p 5173:3000 \
-#   --name medyk-lab \
-#   medyk-lab
-
-#   # -ti \
